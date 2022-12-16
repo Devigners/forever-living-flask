16415dae9f6acfdc17416104958ee307bf045668
Usama Khaki <khakiu.uk@gmail.com>
2022-12-16 11:59:05 +0400

    directory converted from to pages

--- app.py
+++ app.py
@@ -7,31 +7,31 @@ app = Flask(__name__)
 # index page
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('pages/index.html')
 
 
 # shop page
 @app.route('/shop')
 def shop():
-    return render_template('shop.html')
+    return render_template('pages/shop.html')
 
 
 # blog page
 @app.route('/blogs')
 def blogs():
-    return render_template('blogs.html')
+    return render_template('pages/blogs.html')
 
 
 # blog page
 @app.route('/blog-details')
 def blogDetails():
-    return render_template('blog-details.html')
+    return render_template('pages/blog-details.html')
 
 
 # product details
 @app.route('/product/details')
 def productDetails():
-    return render_template('single-product.html')
+    return render_template('pages/single-product.html')
 
 
 if __name__ == '__main__':
