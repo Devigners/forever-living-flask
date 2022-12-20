84ed88e275f629d4a123d99edb440ad3b686aa04
SaifKhaki <saifbinkhaki@gmail.com>
2022-12-20 10:15:29 +0400

    done from saif side

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ from flask import Flask, render_template, request, redirect, url_for
 from werkzeug.utils import secure_filename
 from utilities import *
 import os
-import random
 
 # folders to work with 
 countries_folder = os.path.join('static', 'data', 'countries')
 countries_folder = os.path.join('static', 'data', 'products')
 images_folder = os.path.join('static', 'images')
 countries = ['AU', 'US', 'CA', 'GB']
-all_products = getProducts()
+controller = data()
+all_products = None
 
 # flask app name 
 app = Flask(__name__)
@@ -19,41 +19,51 @@ app.config['COUNTRY_FOLDER'] = countries_folder
 # allowed data file extensions
 ALLOWED_EXTENSIONS = {'csv'}
 
-# index page with country name
-@app.route('/country/<country>', methods=['GET', 'POST'])
-def country(country = 'unitedstates'):
-    return render_template('pages/index.html', categories=getCategories(), products = all_products, productsGroupByCategory = getProductsGroupByCategory(), country=country, localities = findLocalities(country))
-
 # index page
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('pages/index.html', categories=getCategories(), products = all_products, productsGroupByCategory = getProductsGroupByCategory(), country="unitedstates", localities = findLocalities('unitedstates'))
+    global all_products
+    country = 'unitedstates'
+    controller.setCountry(country)
+    all_products = controller.getProducts()
+    
+    return render_template('pages/index.html', categories=controller.getCategories(), products = all_products, productsGroupByCategory = controller.getProductsGroupByCategory(), country=country, localities = controller.findLocalities(country))
 
+# index page with country name
+@app.route('/country/<country>', methods=['GET', 'POST'])
+@app.route('/country/<country>/<restArea>', methods=['GET', 'POST'])
+def country(country, restArea = None):
+    global all_products
+    controller.setCountry(country)
+    all_products = controller.getProducts()
+    
+    return render_template('pages/index.html', categories=controller.getCategories(), products = all_products, productsGroupByCategory = controller.getProductsGroupByCategory(), country=country, localities = controller.findLocalities(country))
 
 # shop page
-@app.route('/shop', methods=['GET', 'POST'])
-def shop():
-    return render_template('pages/shop.html', categories=getCategories(), products=all_products)
+@app.route('/<country>/shop', methods=['GET', 'POST'])
+def shop(country):
+    global all_products
+    return render_template('pages/shop.html', categories=controller.getCategories(), products=all_products, country=country, localities = controller.findLocalities(country))
 
 
 # blog page
-@app.route('/blogs', methods=['GET', 'POST'])
-def blogs():
-    return render_template('pages/blogs.html')
+@app.route('/<country>/blogs', methods=['GET', 'POST'])
+def blogs(country):
+    return render_template('pages/blogs.html',country=country , localities = controller.findLocalities(country))
 
 
 # blog page
-@app.route('/blog-details', methods=['GET', 'POST'])
-def blogDetails():
-    return render_template('pages/blog-details.html')
+@app.route('/<country>/blog-details', methods=['GET', 'POST'])
+def blogDetails(country):
+    return render_template('pages/blog-details.html',country=country , localities = controller.findLocalities(country))
 
 
 # product details
-@app.route('/product/details/<int:id>', methods=['GET', 'POST'])
-def productDetails(id):
+@app.route('/<country>/product/details/<int:id>', methods=['GET', 'POST'])
+def productDetails(country, id):
     print('Category:', all_products[id][10])
-    print('Items:', len(getProductsWithCategory(category=all_products[id][10])))
-    return render_template('pages/single-product.html', product = all_products[id], similar_products = getProductsWithCategory(category=all_products[id][10]), all_products=all_products)
+    print('Items:', len(controller.getProductsWithCategory(category=all_products[id][10])))
+    return render_template('pages/single-product.html', country=country, product = all_products[id], similar_products = controller.getProductsWithCategory(category=all_products[id][10]), all_products=all_products, localities = controller.findLocalities(country))
 
 
 if __name__ == '__main__':
--- utilities.py
+++ utilities.py
@@ -1,59 +1,71 @@
+from flask import Flask, render_template, request, redirect, url_for
 from os import listdir
 import pandas as pd
 
-needed_columns = ['Image', 'post_title', 'Price', 'Description', 'Usage', 'quantities', 'Ingredients', 'Tags', 'review_stars', 'Original URL', 'aus_categories']
-products = pd.read_csv('static/data/products/forever_products_en_au.csv')
-products['review_stars'].fillna(0.0, inplace = True)
-products['total_reviews'].fillna('', inplace = True)
-products['quantities'].fillna('', inplace = True)
-products['Usage'].fillna('', inplace = True)
+class data():
+    def __init__(self) -> None:
+        self.country = None
+        self.needed_columns = None
+        self.products = None
+        self.country_categoryNames = {'australia': 'aus_categories', 'unitedstates':'us_categories', 'canada':'can_categories', 'greatbritain':'uk_categories'}
+        self.country_code = {'australia': 'au', 'unitedstates':'us', 'canada':'ca', 'greatbritain':'GB'}
 
-locality_dict = {}
-
-def getCategories():
-    return list(set(products['aus_categories'].to_list()))
-
-def getProductsCards(return_list=True):
-    if(return_list):
-        return products[['Image', 'post_title', 'Price']].values.tolist()
-    else:
-        return products[['Image', 'post_title', 'Price']]
-
-def getProducts(return_list=True):
-    if(return_list):
-        return products[needed_columns].values.tolist()
-    else:
-        return products[needed_columns]
-
-def getProductsWithCategory(category, return_list=True):
-    specific_category_products = products[products['aus_categories'] == category]
-    if(return_list):
-        return specific_category_products[needed_columns].values.tolist()
-    else:
-        return specific_category_products[needed_columns]
-
-def getProductsGroupByCategory():
-    category_json = {}
-    for category in getCategories():
-        category_json[category] = getProductsWithCategory(category)
-    return category_json
-
-def findLocalities(required_country = 'unitedstates'):
-    countries_codes = {'australia': 'AU', 'unitedstates':'US', 'canada':'CA', 'greatbritain':'GB'}
-    required_country_code = countries_codes[required_country]
-    data = pd.read_csv('static/data/countries/'+required_country_code+' data.csv').values.tolist()
-
-    localities = []
-    for row in data:
-        entry = []
-        for item in row:
-            if isinstance(item, str):
-                entry.append(item)
-        localities.append(entry)
-
-    country_localities = []
-    for locality in localities:
-        # add your search url here
-        country_localities.append({'name': ', '.join(locality), 'url': '#'})
+    def setCountry(self, country):
+        self.country = country
+        self.needed_columns = ['Image', 'post_title', 'Price', 'Description', 'Usage', 'quantities', 'Ingredients', 'Tags', 'review_stars', 'Original URL', self.country_categoryNames[self.country]]
+        self.products = pd.read_csv('static/data/products/forever_products_en_'+self.country_code[self.country]+'.csv')
+        self.products['review_stars'].fillna(0.0, inplace = True)
+        self.products['total_reviews'].fillna('', inplace = True)
+        self.products['quantities'].fillna('', inplace = True)
+        self.products['Usage'].fillna('', inplace = True)
     
-    return country_localities
+    def getCountry(self):
+        return self.country
+    
+    def getCategories(self):
+        return list(set(self.products[self.country_categoryNames[self.country]].to_list()))
+
+    def getProductsCards(self, return_list=True):
+        if(return_list):
+            return self.products[['Image', 'post_title', 'Price']].values.tolist()
+        else:
+            return self.products[['Image', 'post_title', 'Price']]
+
+    def getProducts(self, return_list=True):
+        if(return_list):
+            return self.products[self.needed_columns].values.tolist()
+        else:
+            return self.products[self.needed_columns]
+
+    def getProductsWithCategory(self, category, return_list=True):
+        specific_category_products = self.products[self.products[self.country_categoryNames[self.country]] == category]
+        if(return_list):
+            return specific_category_products[self.needed_columns].values.tolist()
+        else:
+            return specific_category_products[self.needed_columns]
+
+    def getProductsGroupByCategory(self):
+        category_json = {}
+        for category in self.getCategories():
+            category_json[category] = self.getProductsWithCategory(category)
+        return category_json
+
+    def findLocalities(self, required_country):
+        countries_codes_file = {'australia': 'AU', 'unitedstates':'US', 'canada':'CA', 'greatbritain':'GB'}
+        required_country_code = countries_codes_file[required_country]
+        data = pd.read_csv('static//data//countries//'+required_country_code+' data.csv').values.tolist()
+
+        localities = []
+        for row in data:
+            entry = []
+            for item in row:
+                if isinstance(item, str):
+                    entry.append(item)
+            localities.append(entry)
+
+        country_localities = []
+        for locality in localities:
+            # add your search url here
+            country_localities.append({'name': ', '.join(locality), 'url': url_for('country', country=required_country, restArea='-'.join([i.replace(' ','') for i in locality]))})
+        
+        return country_localities
