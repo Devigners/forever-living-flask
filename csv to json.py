30bf7081cee3a6647b65d41d91f0bec3218795fa
SaifKhaki <saifbinkhaki@gmail.com>
2023-01-27 09:19:09 +0400

    Deleting unused files -  creating some CSVs - Bringing back some notebooks

--- csv to json.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv('static\\web-assets\\data\\blogs\\blogs_small.csv')
-print(df)
-
-df.to_json('static\\web-assets\\data\\blogs\\blogs_small.json', orient='index')
