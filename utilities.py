c5b6964ca00ce354087469b9881dc2c27ae3d5a3
SaifKhaki <saifbinkhaki@gmail.com>
2023-01-27 09:42:03 +0400

    Removed special characters from the product title

--- utilities.py
+++ utilities.py
@@ -36,7 +36,7 @@ class data():
         # then, it replace ' ' with -'es
         # then, it replace multiple dashes with single dash
         self.products['url_name'] = [
-            re.sub(r'[ ]+', ' ', i.replace('&', 'and').replace('Â', '').replace('®', '').replace('™', '').replace('-', ' ').title()).replace(' ', '-') for i in self.products['post_title'].values.tolist()]
+            re.sub(r'[ ]+', ' ', i.replace('&', 'and').replace('Â', '').replace('®', '').replace('™', '').replace("'", '').replace('+', '').replace('è', 'e').replace('-', ' ').title()).replace(' ', '-') for i in self.products['post_title'].values.tolist()]
 
         canonical_categories = []
         for product_categories in self.products[self.categories].values.tolist():
